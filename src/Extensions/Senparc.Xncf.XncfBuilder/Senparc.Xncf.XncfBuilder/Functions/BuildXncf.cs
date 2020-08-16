72284ea244bfed06e7cbaa61998832deb947717e
JeffreySu <www.jeffrey.su@gmail.com>
2020-08-16 18:04:27 +0800

    添加 XncfBuilder

---
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Functions/BuildXncf.cs
@@ -0,0 +1,40 @@
+﻿using Senparc.Ncf.XncfBase;
+using Senparc.Ncf.XncfBase.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Senparc.Xncf.XncfBuilder.Functions
+{
+
+    public class BuildXncf : FunctionBase
+    {
+        public BuildXncf(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        public class Parameters : IFunctionParameter
+        {
+            [MaxLength(300)]
+            [Description("模块名称||不能带有空格和.")]
+            public string Name { get; set; }
+        }
+
+
+        public override string Name => "生成 XNCF";
+
+        public override string Description => "根据配置条件生成 XNCF";
+
+        public override Type FunctionParameterType => typeof(Parameters);
+
+        public override FunctionResult Run(IFunctionParameter param)
+        {
+            return FunctionHelper.RunFunction<Parameters>(param, (typeParam, sb, result) =>
+            {
+                //生成
+            });
+        }
+    }
+}
---
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Register.cs
@@ -0,0 +1,29 @@
+﻿using Senparc.Ncf.XncfBase;
+using Senparc.Xncf.XncfBuilder.Functions;
+using System;
+using System.Collections.Generic;
+
+namespace Senparc.Xncf.XncfBuilder
+{
+    [XncfRegister]
+    public partial class Register : XncfRegisterBase, IXncfRegister
+    {
+        #region IRegister 接口
+
+        public override string Name => "Senparc.Xncf.XncfBuilder";
+
+        public override string Uid => "C2E1F87F-2DCE-4921-87CE-36923ED0D6EA";//必须确保全局唯一，生成后必须固定
+
+        public override string Version => "0.1";//必须填写版本号
+
+        public override string MenuName => "XNCF 模块生成器";
+
+        public override string Icon => "fa fa-ofbuilding";
+
+        public override string Description => "快速生成 XNCF 模块基础程序代码";
+
+        public override IList<Type> Functions => new Type[] { typeof(BuildXncf) };
+
+        #endregion
+    }
+}
