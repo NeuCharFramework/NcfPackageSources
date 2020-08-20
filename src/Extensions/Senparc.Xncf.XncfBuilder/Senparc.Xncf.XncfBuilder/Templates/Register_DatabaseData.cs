25827fadeb1b83cbb76c0b3e150d1a775acac69d
JeffreySu <www.jeffrey.su@gmail.com>
2020-08-20 12:21:33 +0800

    添加模板文件

---
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Templates/Register_DatabaseData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Xncf.XncfBuidler.Templates
+{
+    public partial class Register_Database : IXncfTemplatePage
+    {
+        /// <summary>
+        /// 相对地址
+        /// </summary>
+        public string RelativeFilePath => "Register.Database.cs";
+
+        public string OrgName { get; set; }
+        public string XncfName { get; set; }
+
+        public Register_Database(string orgName, string xncfName)
+        {
+            OrgName = orgName;
+            XncfName = xncfName;
+        }
+    }
+}
