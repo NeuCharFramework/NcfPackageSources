e08e63fd70ac7d9d9ec8c99dcf1dc200bd4775b2
JeffreySu <zsu@senparc.com>
2020-10-04 16:40:56 +0800

    添加 Senparc.Ncf.Database.MySql

---
+++ src/Basic/Senparc.Ncf.Database.MySql/MySqlDatabaseConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Senparc.Ncf.Database;
+using Senparc.Ncf.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
+
+namespace Senparc.Ncf.Database.SqlServer
+{
+    public class MySqlDatabaseConfiguration : DatabaseConfigurationBase<MySqlDbContextOptionsBuilder, MySqlOptionsExtension>
+    {
+        public MySqlDatabaseConfiguration() { }
+
+        public override Action<IRelationalDbContextOptionsBuilderInfrastructure, XncfDatabaseData> DbContextOptionsActionExtension => (optionsBuilder, xncfDatabaseData) =>
+        {
+            var typedBuilder = optionsBuilder as MySqlDbContextOptionsBuilder;
+            typedBuilder.EnableRetryOnFailure(
+                       maxRetryCount: 5,
+                       maxRetryDelay: TimeSpan.FromSeconds(5),
+                       errorNumbersToAdd: new int[] { 2 });
+        };
+
+        public override Action<DbContextOptionsBuilder, string, XncfDatabaseData, Action<IRelationalDbContextOptionsBuilderInfrastructure>> SetUseDatabase =>
+            (optionsBuilder, connectionString, xncfDatabaseData, actionBase) =>
+            {
+                optionsBuilder.UseMySql(connectionString, actionBase);//beta6
+            };
+    }
+}
