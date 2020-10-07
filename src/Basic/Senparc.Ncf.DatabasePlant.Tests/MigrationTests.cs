8e9e9f726636d35bb011022cc0469fd6f2d196a9
JeffreySu <zsu@senparc.com>
2020-10-07 22:22:54 +0800

    创建 Senparc.Ncf.DatabasePlant.Tests 单元测试项目

---
+++ src/Basic/Senparc.Ncf.DatabasePlant.Tests/MigrationTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace Senparc.Ncf.DatabasePlant.Tests
+{
+    [TestClass]
+    public class MigrationTests
+    {
+        [TestMethod]
+        public void AddMigrationTest()
+        {
+            var commandTexts = new[] { 
+                @"cd E:\Senparc项目\NeuCharFramework\NcfPackageSources\src\Basic\Senparc.Ncf.DatabasePlant",
+                @"dir",
+                @"add-migration init -C XncfBuilderEntities  -o ""E:\Senparc项目\NeuCharFramework\NcfPackageSources\src\Extensions\Senparc.Xncf.XncfBuilder\Senparc.Xncf.XncfBuilder\Migration.SQLite"""
+
+
+            };
+
+                Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            string strOutput = null;
+            try
+            {
+                p.Start();
+                foreach (string item in commandTexts)
+                {
+                    p.StandardInput.WriteLine(item);
+                }
+                p.StandardInput.WriteLine("exit");
+                strOutput = p.StandardOutput.ReadToEnd();
+                //strOutput = Encoding.UTF8.GetString(Encoding.Default.GetBytes(strOutput));
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception e)
+            {
+                strOutput = e.Message;
+            }
+            Console.WriteLine(strOutput);
+        }
+    }
+}
