e171cb3ce198e5246d8ffa1e4468bcb38ff98c5f
JeffreySu <zsu@senparc.com>
2020-10-10 22:39:03 +0800

    完善多数据库配置（XNCF案例）

--- src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Models/SenparcDbContextFactory_XncfBuilderEntities.cs
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Models/SenparcDbContextFactory_XncfBuilderEntities.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Senparc.CO2NET.Extensions;
-using Senparc.Ncf.Database;
 using Senparc.Ncf.XncfBase.Database;
-using Senparc.Xncf.XncfBuilder.Models.MultipleDatabase;
 using System;
 using System.IO;
 
@@ -11,28 +8,22 @@ namespace Senparc.Xncf.XncfBuilder
     /// <summary>
     /// 设计时 DbContext 创建（仅在开发时创建 Code-First 的数据库 Migration 使用，在生产环境不会执行）
     /// <para>1、切换至 Debug 模式</para>
-    /// <para>2、运行：PM> add-migration [更新名称] -C XncfBuilderEntities_SqlServer -o Migrations/Migrations.SqlServer </para>
+    /// <para>2、运行：PM> add-migration [更新名称] -Context XncfBuilderEntities</para>
     /// </summary>
-    public class SenparcDbContextFactory_SqlServer : SenparcDesignTimeDbContextFactoryBase<XncfBuilderEntities_SqlServer, Register>
+    public class SenparcDbContextFactory_XncfBuilderEntities : SenparcDesignTimeDbContextFactoryBase<XncfBuilderEntities, Register>
     {
         protected override Action<IServiceCollection> ServicesAction => services =>
         {
-            //指定其他数据库
-            services.AddDatabase("Senparc.Ncf.Database.SqlServer", "Senparc.Ncf.Database.SqlServer", "SQLServerDatabaseConfiguration");
+            //services.AddDatabase<SQLServerDatabaseConfiguration>();//指定其他数据库
         };
 
-        public SenparcDbContextFactory_SqlServer()
+        public SenparcDbContextFactory_XncfBuilderEntities()
             : base(
                  /* Debug模式下项目根目录
                  /* 用于寻找 App_Data 文件夹，从而找到数据库连接字符串配置信息 */
                  Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"))
         {
 
-        }
-
-        public override XncfBuilderEntities_SqlServer CreateDbContext(string[] args)
-        {
-            return base.CreateDbContext(args);
         }
     }
 }
