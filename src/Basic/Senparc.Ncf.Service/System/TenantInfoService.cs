fe80917949ebb9e0bb1d07c4f09b828d212ccb51
JeffreySu <zsu@senparc.com>
2021-02-13 22:52:52 +0800

    Add TenantInfoService

---
+++ src/Basic/Senparc.Ncf.Service/System/TenantInfoService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Senparc.Ncf.Core.Models.DataBaseModel;
+using Senparc.Ncf.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Ncf.Service
+{
+    public class TenantInfoService : ClientServiceBase<TenantInfo>
+    {
+        private readonly Lazy<IHttpContextAccessor> httpContextAccessor;
+
+        public TenantInfoService(IClientRepositoryBase<TenantInfo> repo, IServiceProvider serviceProvider, Lazy<IHttpContextAccessor> httpContextAccessor)
+            : base(repo, serviceProvider)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// 创建默认 TenantInfo 信息
+        /// </summary>
+        /// <returns></returns>
+        public async Task<TenantInfo> CreateInitTenantInfo()
+        {
+            var httpContext = httpContextAccessor.Value.HttpContext;
+            var urlData = httpContext.Request;
+            var host = urlData.Host.Host;
+            TenantInfo tenantInfo = new TenantInfo(host, true, host.ToUpper());
+            await SaveObjectAsync(tenantInfo);
+            return tenantInfo;
+        }
+    }
+}
