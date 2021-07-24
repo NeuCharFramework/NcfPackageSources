5549ee85bf617d0ab2e8ade7409ad2c9503dbe1c
JeffreySu <zsu@senparc.com>
2021-07-24 23:07:51 +0800

    Swagger v0.1.2.2-beta3 暂时隐藏数据库

--- src/Extensions/Senparc.Xncf.Swagger/Register.Database.cs
+++ src/Extensions/Senparc.Xncf.Swagger/Register.Database.cs
@@ -7,21 +7,21 @@ using System.Text;
 
 namespace Senparc.Xncf.Swagger
 {
-    public partial class Register : IXncfDatabase
-    {
-        public const string DATABASE_PREFIX = "Swagger";
+    //public partial class Register : IXncfDatabase
+    //{
+    //    public const string DATABASE_PREFIX = "Swagger";
 
-        public string DatabaseUniquePrefix => DATABASE_PREFIX;
-        public Type TryGetXncfDatabaseDbContextType => typeof(SwaggerEntities);
+    //    public string DatabaseUniquePrefix => DATABASE_PREFIX;
+    //    public Type TryGetXncfDatabaseDbContextType => typeof(SwaggerEntities);
 
-        public void AddXncfDatabaseModule(IServiceCollection services)
-        {
-            throw new NotImplementedException();
-        }
+    //    public void AddXncfDatabaseModule(IServiceCollection services)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
 
-        public void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public void OnModelCreating(ModelBuilder modelBuilder)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
