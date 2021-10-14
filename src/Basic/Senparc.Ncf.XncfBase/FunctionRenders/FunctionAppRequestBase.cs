3d762699bb9be9503496646741362d4eb2ae47c4
JeffreySu <zsu@senparc.com>
2021-10-14 14:19:29 +0800

    XncfBase v0.7-beta4.5

--- src/Basic/Senparc.Ncf.XncfBase/FunctionRenders/FunctionAppRequestBase.cs
+++ src/Basic/Senparc.Ncf.XncfBase/FunctionRenders/FunctionAppRequestBase.cs
@@ -4,10 +4,15 @@ using System.Threading.Tasks;
 
 namespace Senparc.Ncf.XncfBase.FunctionRenders
 {
+    public interface IFunctionAppRequest : IAppRequest
+    {
+        Task LoadData(IServiceProvider serviceProvider);
+    }
+
     /// <summary>
     /// FunctionAppRequest 基类
     /// </summary>
-    public class FunctionAppRequestBase : AppRequestBase
+    public class FunctionAppRequestBase : AppRequestBase, IFunctionAppRequest
     {
         public virtual Task LoadData(IServiceProvider serviceProvider)
         {
