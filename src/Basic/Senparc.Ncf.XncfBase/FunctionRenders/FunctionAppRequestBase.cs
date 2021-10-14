bdfb3d78e654e85422776c13435fe759b7265034
JeffreySu <zsu@senparc.com>
2021-10-14 14:43:25 +0800

    重构 FunctionHelper.GetFunctionParameterInfoAsync

--- src/Basic/Senparc.Ncf.XncfBase/FunctionRenders/FunctionAppRequestBase.cs
+++ src/Basic/Senparc.Ncf.XncfBase/FunctionRenders/FunctionAppRequestBase.cs
@@ -1,5 +1,12 @@
-﻿using Senparc.Ncf.Core.AppServices;
+﻿using Senparc.CO2NET.Trace;
+using Senparc.Ncf.Core.AppServices;
+using Senparc.Ncf.XncfBase.Functions;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Senparc.Ncf.XncfBase.FunctionRenders
@@ -18,5 +25,17 @@ namespace Senparc.Ncf.XncfBase.FunctionRenders
         {
             return Task.CompletedTask;
         }
+
+
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="sb"></param>
+        /// <param name="msg"></param>
+        protected void RecordLog(StringBuilder sb, string msg)
+        {
+            FunctionHelper.RecordLog(sb, msg);
+        }
+
     }
 }
