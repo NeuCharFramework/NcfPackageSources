26b048d74e6e2843a21cce8395ca02ef088b852f
JeffreySu <zsu@senparc.com>
2021-11-24 19:40:58 +0800

    迁移数据库实体到 NCF 项目中

--- src/Basic/Senparc.Ncf.Core/Models/SenparcEntities/SenparcEntitiesBase.cs
+++ src/Basic/Senparc.Ncf.Core/Models/SenparcEntities/SenparcEntitiesBase.cs
@@ -13,48 +13,6 @@ namespace Senparc.Ncf.Core.Models
 
         #endregion
 
-        #region 系统表（无特殊情况不要修改）
-
-        /// <summary>
-        /// 系统设置
-        /// </summary>
-        public DbSet<SystemConfig> SystemConfigs { get; set; }
-
-
-        /// <summary>
-        /// 菜单
-        /// </summary>
-        public DbSet<SysMenu> SysMenus { get; set; }
-
-        /// <summary>
-        /// 菜单下面的按钮
-        /// </summary>
-        public DbSet<SysButton> SysButtons { get; set; }
-
-        /// <summary>
-        /// 系统角色
-        /// </summary>
-        public DbSet<SysRole> SysRoles { get; set; }
-
-        /// <summary>
-        /// 角色菜单表
-        /// </summary>
-        public DbSet<SysPermission> SysPermission { get; set; }
-
-        /// <summary>
-        /// 角色人员表
-        /// </summary>
-        public DbSet<SysRoleAdminUserInfo> SysRoleAdminUserInfos { get; set; }
-
-        /// <summary>
-        /// 扩展模块
-        /// </summary>
-        public DbSet<XncfModule> XncfModules { get; set; }
-
-
-        #endregion
-
-
         protected SenparcEntitiesBase(DbContextOptions options, IServiceProvider serviceProvider) : base(options, serviceProvider)
         {
         }
