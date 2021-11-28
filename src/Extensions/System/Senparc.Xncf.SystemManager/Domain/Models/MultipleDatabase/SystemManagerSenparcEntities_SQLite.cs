b0732bdc97dc0958054da16d59de50832e64711a
JeffreySu <zsu@senparc.com>
2021-11-28 16:54:14 +0800

    添加 SystemManager 项目

---
+++ src/Extensions/System/Senparc.Xncf.SystemManager/Domain/Models/MultipleDatabase/SystemManagerSenparcEntities_SQLite.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.Ncf.Database;
+using Senparc.Ncf.Core.Models;
+using Senparc.Ncf.XncfBase.Database;
+using System;
+using System.IO;
+
+namespace Senparc.Xncf.SystemManager.Models
+{
+    [MultipleMigrationDbContext(MultipleDatabaseType.Sqlite, typeof(Register))]
+    public class SystemManagerSenparcEntities_Sqlite : SystemManagerSenparcEntities
+    {
+        public SystemManagerSenparcEntities_Sqlite(DbContextOptions<SystemManagerSenparcEntities_Sqlite> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// 设计时 DbContext 创建（仅在开发时创建 Code-First 的数据库 Migration 使用，在生产环境不会执行）
+    /// <para>1、切换至 Debug 模式</para>
+    /// <para>2、运行：PM> add-migration [更新名称] -c SystemManagerSenparcEntities_Sqlite -o Domain/Migrations/Migrations.Sqlite </para>
+    /// </summary>
+    public class SenparcDbContextFactory_Sqlite : SenparcDesignTimeDbContextFactoryBase<SystemManagerSenparcEntities_Sqlite, Register>
+    {
+        protected override Action<IServiceCollection> ServicesAction => services =>
+        {
+            //指定其他数据库
+            services.AddDatabase("Senparc.Ncf.Database.Sqlite", "Senparc.Ncf.Database.Sqlite", "SqliteMemoryDatabaseConfiguration");
+        };
+
+        public SenparcDbContextFactory_Sqlite() : base(SenparcDbContextFactoryConfig.RootDictionaryPath)
+        {
+
+        }
+    }
+}
