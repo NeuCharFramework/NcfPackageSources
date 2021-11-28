7f2a2a43f15ab6eb72893d34d84a74e800cb0f69
JeffreySu <zsu@senparc.com>
2021-11-28 16:21:39 +0800

    独立 Xncf.Menu模块

---
+++ src/Extensions/System/Senparc.Xncf.Menu/Domain/Migrations/Migrations.Sqlite/20211128080352_Init.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senparc.Xncf.Menu.Domain.Migrations.Migrations.Sqlite
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SysButtons",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    MenuId = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    ButtonName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    OpearMark = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Url = table.Column<string>(type: "TEXT", maxLength: 350, nullable: true),
+                    Flag = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TenantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AdminRemark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true),
+                    Remark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysButtons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SysMenus",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    MenuName = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    ParentId = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Url = table.Column<string>(type: "TEXT", maxLength: 350, nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    IsLocked = table.Column<bool>(type: "INTEGER", nullable: false),
+                    MenuType = table.Column<int>(type: "INTEGER", nullable: false),
+                    ResourceCode = table.Column<string>(type: "TEXT", maxLength: 30, nullable: true),
+                    Sort = table.Column<int>(type: "INTEGER", nullable: false),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Flag = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TenantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AdminRemark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true),
+                    Remark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SysMenus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SysButtons");
+
+            migrationBuilder.DropTable(
+                name: "SysMenus");
+        }
+    }
+}
