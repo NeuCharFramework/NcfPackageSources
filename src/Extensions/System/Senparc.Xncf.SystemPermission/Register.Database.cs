b56972508f54b1e4a0032489b2441695be5ca723
zhao365845726@163.com <zhao365845726@163.com>
2021-11-28 20:44:38 +0800

    增加权限模块

---
+++ src/Extensions/System/Senparc.Xncf.SystemPermission/Register.Database.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.Ncf.Core.Models;
+using Senparc.Ncf.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Xncf.SystemPermission
+{
+    public partial class Register : IXncfDatabase
+    {
+        public const string DATABASE_PREFIX = NcfDatabaseMigrationHelper.SYSTEM_UNIQUE_PREFIX;//系统表，
+
+        public string DatabaseUniquePrefix => DATABASE_PREFIX;
+
+        public Type TryGetXncfDatabaseDbContextType => MultipleDatabasePool.Instance.GetXncfDbContextType(this);
+
+        public void AddXncfDatabaseModule(IServiceCollection services)
+        {
+
+        }
+
+        public void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
---
+++ src/Extensions/System/Senparc.Xncf.SystemPermission/Register.cs
@@ -0,0 +1,45 @@
+﻿using Senparc.Ncf.Core.Enums;
+using Senparc.Ncf.XncfBase;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Senparc.Xncf.SystemPermission
+{
+    [XncfRegister]
+    public partial class Register : XncfRegisterBase, IXncfRegister
+    {
+        #region IXncfRegister 接口
+
+        public override string Name => "Senparc.Xncf.SystemPermission";
+
+        public override string Uid => "0E7E2D66-DCAA-46B1-AADE-10935516FC3B";//必须确保全局唯一，生成后必须固定，已自动生成，也可自行修改
+
+        public override string Version => "0.1";//必须填写版本号
+
+        public override string MenuName => "权限管理";
+
+        public override string Icon => "fa fa-bars";
+
+        public override string Description => "系统权限管理";
+
+        public override async Task InstallOrUpdateAsync(IServiceProvider serviceProvider, InstallOrUpdate installOrUpdate)
+        {
+        }
+
+        public override async Task UninstallAsync(IServiceProvider serviceProvider, Func<Task> unsinstallFunc)
+        {
+            await unsinstallFunc().ConfigureAwait(false);
+        }
+        #endregion
+
+        public override IServiceCollection AddXncfModule(IServiceCollection services, IConfiguration configuration)
+        {
+            return base.AddXncfModule(services, configuration);
+        }
+    }
+}
