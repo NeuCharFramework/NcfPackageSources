851e565b855661a058861d116b3971ea4f3e60b7
JeffreySu <zsu@senparc.com>
2021-12-11 21:41:52 +0800

    添加XncfModuleManager的数据库迁移文件

---
+++ src/Extensions/System/Senparc.Xncf.XncfModuleManager/Domain/Migrations/Mysql/20211211134009_Init.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senparc.Xncf.XncfModuleManager.Domain.Migrations.Mysql
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "XncfModules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Uid = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MenuName = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Version = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UpdateLog = table.Column<string>(type: "text", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AllowRemove = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    MenuId = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Icon = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    Flag = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    AdminRemark = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Remark = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_XncfModules", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "XncfModules");
+        }
+    }
+}
---
+++ src/Extensions/System/Senparc.Xncf.XncfModuleManager/Domain/Migrations/Sqlite/20211211133958_Init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senparc.Xncf.XncfModuleManager.Domain.Migrations.Sqlite
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "XncfModules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Uid = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    MenuName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Version = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    UpdateLog = table.Column<string>(type: "ntext", nullable: false),
+                    AllowRemove = table.Column<bool>(type: "INTEGER", nullable: false),
+                    MenuId = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Icon = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    State = table.Column<int>(type: "INTEGER", nullable: false),
+                    Flag = table.Column<bool>(type: "INTEGER", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TenantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AdminRemark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true),
+                    Remark = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_XncfModules", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "XncfModules");
+        }
+    }
+}
