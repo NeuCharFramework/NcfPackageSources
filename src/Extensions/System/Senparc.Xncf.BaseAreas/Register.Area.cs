6a83e5a1cfd621ded6cacd2b272296b862268007
JeffreySu <zsu@senparc.com>
2021-12-15 21:28:23 +0800

    添加 BaseAreas 项目

---
+++ src/Extensions/System/Senparc.Xncf.BaseAreas/Register.Area.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Senparc.Ncf.Core.Areas;
+using System.Collections.Generic;
+
+namespace Senparc.Xncf.BaseAreas
+{
+    public partial class Register : IAreaRegister
+    {
+        public string HomeUrl => "";
+
+        public List<AreaPageMenuItem> AareaPageMenuItems => new List<AreaPageMenuItem>();
+
+        public IMvcBuilder AuthorizeConfig(IMvcBuilder builder, IHostEnvironment env)
+        {
+            builder.Services.AddRazorPages(opt =>
+            {
+                //opt.RootDirectory = "/";
+            })
+              .AddNcfAreas(env)//注册所有 Ncf 的 Area 模块（必须）
+              .AddXmlSerializerFormatters()
+              .AddJsonOptions(options =>
+              {
+                  //忽略循环引用
+                  //options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                  //不使用驼峰样式的key
+                  //options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                  //设置时间格式
+                  //options.SerializerSettings.DateFormatString = "yyyy-MM-dd";
+              })
+              //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/app-state?view=aspnetcore-2.1&tabs=aspnetcore2x
+              //.AddSessionStateTempDataProvider()
+              //忽略JSON序列化过程中的循环引用：https://stackoverflow.com/questions/7397207/json-net-error-self-referencing-loop-detected-for-type
+              .AddRazorPagesOptions(options =>
+              {
+                  //自动注册  防止跨站请求伪造（XSRF/CSRF）攻击
+                  options.Conventions.Add(new Senparc.Ncf.AreaBase.Conventions.AutoValidateAntiForgeryTokenModelConvention());
+              });
+            return builder;
+        }
+    }
+}
---
+++ src/Extensions/System/Senparc.Xncf.BaseAreas/Register.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Senparc.CO2NET.RegisterServices;
+using Senparc.Ncf.Core.Config;
+using Senparc.Ncf.Core.Enums;
+using Senparc.Ncf.XncfBase;
+using System;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.BaseAreas
+{
+    [XncfRegister]
+    [XncfOrder(5955)]
+    public partial class Register : XncfRegisterBase, IXncfRegister
+    {
+        #region IXncfRegister 接口
+
+        public override string Name => "Senparc.Xncf.BaseAreas";
+
+        public override string Uid => SiteConfig.SYSTEM_XNCF_BASE_AREAS;// "00000000-0000-0000-0001-000000000006";
+
+        public override string Version => "0.1";//必须填写版本号
+
+        public override string MenuName => "Areas 基础模块";
+
+        public override string Icon => "fa fa-university";//fa fa-cog
+
+        public override string Description => "运行 RazorPage 等页面的基础模块";
+
+        public override async Task InstallOrUpdateAsync(IServiceProvider serviceProvider, InstallOrUpdate installOrUpdate)
+        {
+
+            await base.InstallOrUpdateAsync(serviceProvider, installOrUpdate);
+        }
+
+        public override async Task UninstallAsync(IServiceProvider serviceProvider, Func<Task> unsinstallFunc)
+        {
+            await base.UninstallAsync(serviceProvider, unsinstallFunc);
+        }
+        #endregion
+
+        public override IServiceCollection AddXncfModule(IServiceCollection services, IConfiguration configuration)
+        {
+
+#if DEBUG
+            //Razor启用运行时编译，多个项目不需要手动编译。
+            if (env.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation(options =>
+                {
+                    //自动索引所有需要使用 RazorRuntimeCompilation 的模块
+                    foreach (var razorRegister in XncfRegisterManager.RegisterList.Where(z => z is IXncfRazorRuntimeCompilation))
+                    {
+                        try
+                        {
+                            var libraryPath = ((IXncfRazorRuntimeCompilation)razorRegister).LibraryPath;
+                            options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+                        }
+                        catch (Exception ex)
+                        {
+                            SenparcTrace.BaseExceptionLog(ex);
+                        }
+                    }
+                });
+            }
+#endif
+            return base.AddXncfModule(services, configuration);
+        }
+
+        public override IApplicationBuilder UseXncfModule(IApplicationBuilder app, IRegisterService registerService)
+        {
+            return base.UseXncfModule(app, registerService);
+        }
+    }
+}
