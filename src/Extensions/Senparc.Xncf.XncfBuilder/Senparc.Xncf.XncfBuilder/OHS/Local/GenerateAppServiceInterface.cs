4f9f09e77710a8006da129ca11dee01ce432261c
JeffreySu <zsu@senparc.com>
2022-02-05 21:33:44 +0800

    添加 GenerateAppServiceInterface 方法

---
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/OHS/Local/GenerateAppServiceInterface.cs
@@ -0,0 +1,31 @@
+﻿using Senparc.Ncf.Core.AppServices;
+using Senparc.Xncf.XncfBuilder.OHS.PL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.XncfBuilder.OHS.Local
+{
+
+    public class GenerateAppServiceInterface : AppServiceBase
+    {
+        public GenerateAppServiceInterface(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        /// <summary>
+        /// 生成 生成 AppService 接口代码
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [FunctionRender("生成 AppService 接口代码", "将某个模块的 AppService ", typeof(Register))]
+        public async Task<StringAppResponse> Generate(GenerateAppServiceInterface_GenerateRequest request)
+        {
+            return await this.GetResponseAsync<StringAppResponse, string>(async (response, logger) =>
+            {
+                return "";
+            });
+        }
+    }
+}
---
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/OHS/PL/GenerateAppServiceInterfaceRequest.cs
@@ -0,0 +1,30 @@
+﻿using Senparc.Ncf.XncfBase.FunctionRenders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Senparc.Xncf.XncfBuilder.OHS.PL
+{
+    public class GenerateAppServiceInterface_GenerateRequest: FunctionAppRequestBase
+    {
+        [Required]
+        [Description("目标项目路径||需要生成接口的项目路径，如：E:\\Senparc项目\\NeuCharFramework\\NCF\\src\\back-end\\Senparc.Xncf.Installer")]
+        public string TargetProjectPath { get; set; }
+
+        [Required]
+        [Description("命名空间前缀||需要生成的类的命名空间前缀，如：Senparc.Xncf.Installer")]
+        public string NamespacePrefix { get; set; }
+
+        [Description("类名正则||需要匹配的类名的正则表达式")]
+        public string ClassNamePattern { get; set; }
+
+        [Description("方法名正则||需要匹配的类名的方法名正则表达式")]
+        public string MethodNamePattern { get; set; }
+
+        [Required]
+        [Description("生成目的地项目路径||需要生成接口的项目路径，如：E:\\Senparc项目\\NeuCharFramework\\NCF\\src\\back-end\\Senparc.Xncf.MyNewProject")]
+        public string DestinationProjectPath { get; set; }
+    }
+}
