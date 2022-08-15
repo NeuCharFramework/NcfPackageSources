637b4cbe2883cbda49f176fb2d952e8bb1334e9e
JeffreySu <zsu@senparc.com>
2022-08-15 21:38:43 +0800

    Oracle v0.1.0-beta2 添加 SetUseOracleSQLCompatibility() 方法，自动设置 Oracle 版本号

--- src/Basic/Senparc.Ncf.Database.Oracle/OracleDatabaseConfiguration.cs
+++ src/Basic/Senparc.Ncf.Database.Oracle/OracleDatabaseConfiguration.cs
@@ -10,14 +10,28 @@ using Senparc.Ncf.Core.Models;
 using System.Data.Common;
 using Oracle.EntityFrameworkCore.Infrastructure;
 using Oracle.EntityFrameworkCore.Infrastructure.Internal;
+using Senparc.CO2NET.Extensions;
 
 namespace Senparc.Ncf.Database.Oracle
 {
     /// <summary>
-    /// SQL Server 数据库配置
+    /// Oracle 数据库配置
+    /// <para>注意：如果使用 Oracle 12 以下的版本（11），需要在调用 <code>services.AddDatabase&lt;OracleDatabaseConfiguration&gt;();</code> 之前，使用 <code>SetUseOracleSQLCompatibility(string useOracleSQLCompatibility)</code> 方法设置版本号，如 11.2，则输入 "11"</para>
     /// </summary>
     public class OracleDatabaseConfiguration : DatabaseConfigurationBase<OracleDbContextOptionsBuilder, OracleOptionsExtension>
     {
+        private static string _useOracleSQLCompatibility = null;
+
+        /// <summary>
+        /// 设置 UseOracleSQLCompatibility 的参数，如 11.2g，输入"11"，12g，输入"12"（默认为 >= 12，此时可不输入）
+        /// <para>注意：此设置应该在执行 <code>.AddDatabase&lt;OracleDatabaseConfiguration&gt;();</code> 之前执行</para>
+        /// </summary>
+        /// <param name="useOracleSQLCompatibility"></param>
+        public static void SetUseOracleSQLCompatibility(string useOracleSQLCompatibility)
+        {
+            _useOracleSQLCompatibility = useOracleSQLCompatibility;
+        }
+
         public OracleDatabaseConfiguration() { }
 
         public override MultipleDatabaseType MultipleDatabaseType => MultipleDatabaseType.Oracle;
@@ -28,7 +42,10 @@ namespace Senparc.Ncf.Database.Oracle
 
             //This method accepts either a value of "11" or "12" (default). By default, generated SQL is compatible with database version 12 and higher. Customers using Oracle Database version 11.2 should set UseOracleSQLCompatibility("11").
             //https://docs.oracle.com/en/database/oracle/oracle-data-access-components/19.3/odpnt/EFCoreAPI.html#GUID-66247629-2670-44AA-AC55-849C367852AF
-            //typedBuilder.UseOracleSQLCompatibility("11");
+            if (!_useOracleSQLCompatibility.IsNullOrEmpty())
+            {
+                typedBuilder.UseOracleSQLCompatibility(_useOracleSQLCompatibility);
+            }
         };
 
         public override Action<DbContextOptionsBuilder, string, XncfDatabaseData, Action<IRelationalDbContextOptionsBuilderInfrastructure>> SetUseDatabase =>
