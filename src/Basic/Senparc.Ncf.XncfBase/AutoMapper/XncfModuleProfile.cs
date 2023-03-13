bcca3a7db112f92741dceeb44b1bf4a3e8ff662b
JeffreySu <zsu@senparc.com>
2023-03-14 00:59:28 +0800

    添加注释

--- src/Basic/Senparc.Ncf.XncfBase/AutoMapper/XncfModuleProfile.cs
+++ src/Basic/Senparc.Ncf.XncfBase/AutoMapper/XncfModuleProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Senparc.CO2NET.Trace;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,12 +11,16 @@ namespace Senparc.Ncf.XncfBase.AutoMapper
     /// </summary>
     public class XncfModuleProfile : Profile
     {
+
         public XncfModuleProfile()
         {
+            //Console.WriteLine("XncfModuleProfile, XncfRegisterManager.RegisterList Count:" + XncfRegisterManager.RegisterList.Count);
             foreach (var register in XncfRegisterManager.RegisterList)
             {
+                //Console.WriteLine("register:" + register.Name);
                 if (register.AutoMapMappingConfigs != null)
                 {
+                    //Console.WriteLine("register.AutoMapMappingConfigs:" + register.Name + "/" + register.AutoMapMappingConfigs.Count);
                     foreach (var config in register.AutoMapMappingConfigs)
                     {
                         config(this);
