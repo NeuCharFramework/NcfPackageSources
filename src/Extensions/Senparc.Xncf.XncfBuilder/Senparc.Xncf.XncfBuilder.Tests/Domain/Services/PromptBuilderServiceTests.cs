37b4b7780d35d310c37fea4e1dd67478d08dbb6f
苏震巍 <zsu@senparc.com>
2023-10-04 02:09:20 +0800

    优化单元测试

--- src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder.Tests/Domain/Services/PromptBuilderServiceTests.cs
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder.Tests/Domain/Services/PromptBuilderServiceTests.cs
@@ -26,9 +26,7 @@ namespace Senparc.Xncf.XncfBuilder.Domain.Services.Tests
         {
             var entityName = "MyClass";
 
-            var input = $"这个领域用于控制所有的 Prompt 核心业务逻辑，包括使用 Prompt 操作大预言模型所需的所有必要的参数，类名叫：{entityName}，用于管理一组相关联的 Prompt，并统一配置其参数。其中必须要包含 LLM 被调用时的所需的所有参数，包括但不仅限于： MaxToken、Temperature、TopP、FrequencyPenalty、ResultsPerPrompt、StopSequences、ChatSystemPrompt、TokenSelectionBiases，等等，除此以外，还需要包含用于评估 Prompt 效果所需要的必要参数，以及 Name 等属于“Group”类型的实体类应该有的参数。";
-
-
+            var input = $"这个领域用于控制所有的 Prompt 核心业务逻辑，包括使用 Prompt 操作大预言模型所需的所有必要的参数，类名叫：{entityName}，用于管理一组相关联的 Prompt，并统一配置其参数。生成的属性中需要包含常规的 LLM 被调用时的所需的参数，尽可能完整，包括但不仅限于： MaxToken、Temperature、TopP、FrequencyPenalty、ResultsPerPrompt、StopSequences、ChatSystemPrompt、TokenSelectionBiases，等等；除此以外，属性还需要包含用于评估 Prompt 效果所需要的必要参数，以及 Name 等常规实体类应该有的参数。";
 
             var projectPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "XncfBuilderTest");
 
@@ -41,7 +39,7 @@ namespace Senparc.Xncf.XncfBuilder.Domain.Services.Tests
 
             var promptGroupFilePath = Path.Combine(projectPath, $"{entityName}.cs");
             Assert.IsTrue(File.Exists(promptGroupFilePath));
-            Assert.IsTrue(File.Exists(Path.Combine(projectPath, $"Dto/{entityName}.cs")));
+            Assert.IsTrue(File.Exists(Path.Combine(projectPath, $"Dto/{entityName}Dto.cs")));
 
             var promptGroupFileContent = File.ReadAllText(promptGroupFilePath);
             Assert.IsTrue(promptGroupFileContent.Contains($"public class {entityName}"));
