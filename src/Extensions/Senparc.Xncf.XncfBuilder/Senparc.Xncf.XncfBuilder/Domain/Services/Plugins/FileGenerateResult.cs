fce5d245363ac1a470f3adc59ea4b3346323f7e4
苏震巍 <zsu@senparc.com>
2023-10-04 02:27:00 +0800

    更新注释

--- src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Domain/Services/Plugins/FileGenerateResult.cs
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder/Domain/Services/Plugins/FileGenerateResult.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace Senparc.Xncf.XncfBuilder.Domain.Services.Plugins
 {
+    /// <summary>
+    /// 文件生成结果
+    /// </summary>
     public class FileGenerateResult
     {
+        /// <summary>
+        /// 文件名（可能会同时包含路径）
+        /// </summary>
         public string FileName { get; set; }
+        /// <summary>
+        /// 代码或文件内容
+        /// </summary>
         public string EntityCode { get; set; }
     }
 }
