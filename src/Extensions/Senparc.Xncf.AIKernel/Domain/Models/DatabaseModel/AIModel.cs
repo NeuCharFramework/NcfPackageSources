085dc04f5e4e2d6d4887b4fa877305cbd08ff843
苏震巍 <zsu@senparc.com>
2023-12-20 19:08:30 +0800

    添加 Senparc.Xncf.AIKernel 模块

---
+++ src/Extensions/Senparc.Xncf.AIKernel/Domain/Models/DatabaseModel/AIModel.cs
@@ -0,0 +1,172 @@
+using Senparc.Ncf.Core.Models;
+using Senparc.Xncf.AIKernel.Domain.Models.DatabaseModel.Dto;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Senparc.AI;
+
+namespace Senparc.Xncf.AIKernel.Models
+{
+    /// <summary>
+    /// LlmModel 数据库实体
+    /// </summary>
+    [Table(Register.DATABASE_PREFIX + nameof(AIModel))] //必须添加前缀，防止全系统中发生冲突
+    [Serializable]
+    public class AIModel : EntityBase<int>
+    {
+        /// <summary>
+        /// 名称（必须）
+        /// </summary>
+        [Required, MaxLength(100)]
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Endpoint（必须）
+        /// </summary>
+        [Required, MaxLength(250)]
+        public string Endpoint { get; private set; }
+
+        /// <summary>
+        /// 模型的类型（必须）, 例如：OpenAI,Azure OpenAI,HuggingFace
+        /// </summary>
+        [Required, MaxLength(20)]
+        public string ModelType { get; private set; }
+
+        /// <summary>
+        /// OrganizationId（可选）
+        /// </summary>
+        [MaxLength(200)]
+        public string OrganizationId { get; private set; }
+
+        /// <summary>
+        /// ApiKey（可选）
+        /// </summary>
+        [MaxLength(200)]
+        public string ApiKey { get; private set; }
+
+        /// <summary>
+        /// ApiVersion（可选）
+        /// </summary>
+        [MaxLength(100)]
+        public string ApiVersion { get; private set; }
+
+        /// <summary>
+        /// Note（可选）
+        /// </summary>
+        public string Note { get; private set; }
+
+        /// <summary>
+        /// MaxToken（可选）
+        /// </summary>
+        [Required, DefaultValue(0)]
+        public int MaxToken { get; private set; }
+
+        // /// <summary>
+        // /// TextCompletionModelName（可选）
+        // /// </summary>
+        // [MaxLength(100)]
+        // public string TextCompletionModelName { get; private set; }
+        //
+        // /// <summary>
+        // /// TextEmbeddingModelName（可选）
+        // /// </summary>
+        // [MaxLength(100)]
+        // public string TextEmbeddingModelName { get; private set; }
+        //
+        // /// <summary>
+        // /// OtherModelName（可选）
+        // /// </summary>
+        // [MaxLength(100)]
+        // public string OtherModelName { get; private set; }
+
+        /// <summary>
+        /// 是否展示
+        /// </summary>
+        public bool Show { get; private set; }
+
+        private AIModel()
+        {
+        }
+
+        public AIModel(string name, string endpoint, string modelType, string organizationId, string apiKey,
+            string apiVersion, string note, int maxToken)
+        {
+            Name = name;
+            Endpoint = endpoint;
+            ModelType = modelType;
+            OrganizationId = organizationId;
+            ApiKey = apiKey;
+            ApiVersion = apiVersion;
+            Note = note;
+            MaxToken = maxToken;
+        }
+
+        // public LlmModel(string name, string endpoint, string modelType, string organizationId, string apiKey,
+        //     string apiVersion, string note, int maxToken, string textCompletionModelName, string textEmbeddingModelName,
+        //     string otherModelName)
+        // {
+        //     Name = name;
+        //     Endpoint = endpoint;
+        //     ModelType = modelType;
+        //     OrganizationId = organizationId;
+        //     ApiKey = apiKey;
+        //     ApiVersion = apiVersion;
+        //     Note = note;
+        //     MaxToken = maxToken;
+        //     TextCompletionModelName = textCompletionModelName;
+        //     TextEmbeddingModelName = textEmbeddingModelName;
+        //     OtherModelName = otherModelName;
+        // }
+
+        //public LlmModel(string name, string endpoint, string organizationId, string apiKey, string apiVersion, string note, int maxToken, string textCompletionModelName, string textEmbeddingModelName, string otherModelName)
+        //{
+        //    Name = name;
+        //    Endpoint = endpoint;
+        //    OrganizationId = organizationId;
+        //    ApiKey = apiKey;
+        //    ApiVersion = apiVersion;
+        //    Note = note;
+        //    MaxToken = maxToken;
+        //    TextCompletionModelName = textCompletionModelName;
+        //    TextEmbeddingModelName = textEmbeddingModelName;
+        //    OtherModelName = otherModelName;
+        //}
+
+        public AIModel(AIModelDto llmModelDto)
+        {
+            Name = llmModelDto.Name;
+            Endpoint = llmModelDto.Endpoint;
+            OrganizationId = llmModelDto.OrganizationId;
+            ApiKey = llmModelDto.ApiKey;
+            ApiVersion = llmModelDto.ApiVersion;
+            Note = llmModelDto.Note;
+            MaxToken = llmModelDto.MaxToken;
+            // TextCompletionModelName = llmModelDto.TextCompletionModelName;
+            // TextEmbeddingModelName = llmModelDto.TextEmbeddingModelName;
+            // OtherModelName = llmModelDto.OtherModelName;
+        }
+
+        public AIModel Switch(bool show)
+        {
+            Show = show;
+            return this;
+        }
+
+        public AIModel Update(string name, bool show)
+        {
+            this.Name = name;
+            return Switch(show);
+        }
+
+        public string GetModelId()
+        {
+            if (string.IsNullOrWhiteSpace(this.Name))
+            {
+                return "text-davinci-003";
+            }
+
+            return this.Name.Contains("azure") ? this.Name.Substring("azure-".Length) : this.Name;
+        }
+    }
+}
