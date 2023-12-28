bb05c1b80bed2f99e4b4cfde7d8e3e1cde9c74c5
苏震巍 <zsu@senparc.com>
2023-12-28 23:43:31 +0800

    添加初始化数据库

---
+++ src/Extensions/Senparc.Xncf.AIKernel/Domain/Migrations/Migrations.SqlServer/20231228153722_Init.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senparc.Xncf.AIKernel.Domain.Migrations.Migrations.SqlServer
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Senparc_AIKernel_AIModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Endpoint = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    AiPlatform = table.Column<int>(type: "int", nullable: false),
+                    OrganizationId = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ApiKey = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    ApiVersion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxToken = table.Column<int>(type: "int", nullable: false),
+                    Show = table.Column<bool>(type: "bit", nullable: false),
+                    Flag = table.Column<bool>(type: "bit", nullable: false),
+                    AddTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    AdminRemark = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Remark = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Senparc_AIKernel_AIModel", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Senparc_AIKernel_AIModel");
+        }
+    }
+}
