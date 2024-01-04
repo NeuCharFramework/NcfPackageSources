3ddc8addb01c9968a425111396e1d8e18e511754
FelixJ <yjiang@senparc.com>
2024-01-04 11:24:10 +0800

    新增 oracle,postgreSql, sqlite 的migration文件

--- src/Extensions/Senparc.Xncf.AIKernel/Domain/Migrations/Migrations.PostgreSQL/20240104032250_AddAlias.Designer.cs
+++ src/Extensions/Senparc.Xncf.AIKernel/Domain/Migrations/Migrations.PostgreSQL/20240104032250_AddAlias.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Senparc.Xncf.AIKernel.Models;
@@ -11,9 +12,11 @@ using Senparc.Xncf.AIKernel.Models;
 namespace Senparc.Xncf.AIKernel.Domain.Migrations.Migrations.PostgreSQL
 {
     [DbContext(typeof(AIKernelSenparcEntities_PostgreSQL))]
-    partial class AIKernelSenparcEntities_PostgreSQLModelSnapshot : ModelSnapshot
+    [Migration("20240104032250_AddAlias")]
+    partial class AddAlias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
