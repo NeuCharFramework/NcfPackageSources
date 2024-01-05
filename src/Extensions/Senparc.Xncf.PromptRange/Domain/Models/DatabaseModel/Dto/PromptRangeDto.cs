f4e0e514c28528c68e28deabb146254d22265ec8
FelixJ <yjiang@senparc.com>
2024-01-05 20:39:36 +0800

    靶场列表查询返回ID
    Signed-off-by: FelixJ <yjiang@senparc.com>

--- src/Extensions/Senparc.Xncf.PromptRange/Domain/Models/DatabaseModel/Dto/PromptRangeDto.cs
+++ src/Extensions/Senparc.Xncf.PromptRange/Domain/Models/DatabaseModel/Dto/PromptRangeDto.cs
@@ -5,18 +5,22 @@ namespace Senparc.Xncf.PromptRange.Domain.Models.DatabaseModel.Dto;
 public class PromptRangeDto : DtoBase
 {
     /// <summary>
+    /// 主键 ID
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
     /// 靶场代号（用户自定义）
     /// </summary>
-    public string Alias { get;  set; }
+    public string Alias { get; set; }
 
     /// <summary>
     /// 靶场名称（来自版号生成）
     /// </summary>
-    public string RangeName { get;  set; }
+    public string RangeName { get; set; }
 
     // /// <summary>
     // /// 期望结果Json
     // /// </summary>
     // public string ExpectedResultsJson { get;  set; }
-    
 }
