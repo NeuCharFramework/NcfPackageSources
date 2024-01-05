7d6fe7f0a657d7cdf69981a6c713129c943a61e6
FelixJ <yjiang@senparc.com>
2024-01-05 21:00:53 +0800

    add-migrations
    Signed-off-by: FelixJ <yjiang@senparc.com>

---
+++ src/Extensions/Senparc.Xncf.PromptRange/Domain/Migrations/Migrations.SqlServer/20240105123220_DelExpRes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Senparc.Xncf.PromptRange.Domain.Migrations.Migrations.SqlServer
+{
+    /// <inheritdoc />
+    public partial class DelExpRes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedResultsJson",
+                table: "Senparc_PromptRange_PromptRange");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpectedResultsJson",
+                table: "Senparc_PromptRange_PromptItem",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedResultsJson",
+                table: "Senparc_PromptRange_PromptItem");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpectedResultsJson",
+                table: "Senparc_PromptRange_PromptRange",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+    }
+}
