ba2b37008833319139534806285b780b12e561cf
苏震巍 <zsu@senparc.com>
2024-01-15 23:31:58 +0800

    优化示例项目，穿透 Function 显示

--- src/DevelopmentTools/Senparc.IntegrationSample/Pages/Index.cshtml.cs
+++ src/DevelopmentTools/Senparc.IntegrationSample/Pages/Index.cshtml.cs
@@ -6,23 +6,80 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Senparc.Ncf.XncfBase;
+using Senparc.Ncf.XncfBase.FunctionRenders;
+using Senparc.Ncf.XncfBase.Functions;
 
 namespace Senparc.IntegrationSample.Pages
 {
+    public class XncfRegisterItem
+    {
+        public IXncfRegister XncfRegister { get; set; }
+        public List<RegisterFunctionInfo> RegisterFunctionInfoList { get; set; }
+
+
+        public class RegisterFunctionInfo
+        {
+            public string Key { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public FunctionRenderBag FunctionRenderBag { get; set; }
+            public List<FunctionParameterInfo> FunctionParameterInfoList { get; set; }
+        }
+    }
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IServiceProvider _serviceProvider;
 
-        public IList<IXncfRegister> XncfRegisterList { get; set; }
+        public IList<XncfRegisterItem> XncfRegisterList { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, IServiceProvider serviceProvider)
         {
             _logger = logger;
+            this._serviceProvider = serviceProvider;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            XncfRegisterList = Senparc.Ncf.XncfBase.XncfRegisterManager.RegisterList;
+            XncfRegisterList = new List<XncfRegisterItem>();
+
+            //所有Register
+            var xncfRegisterList = Senparc.Ncf.XncfBase.XncfRegisterManager.RegisterList
+                                     .OrderByDescending(z => (z.GetType().GetCustomAttributes(typeof(XncfOrderAttribute), true).FirstOrDefault() as XncfOrderAttribute)?.Order)
+                                     .ToList();
+
+            foreach (var item in xncfRegisterList)
+            {
+                XncfRegisterItem registerItem = new();
+                registerItem.XncfRegister = item;
+                registerItem.RegisterFunctionInfoList = new List<XncfRegisterItem.RegisterFunctionInfo>();
+
+                FunctionRenderBag? rightFunctionBag = null;
+                if (Senparc.Ncf.XncfBase.Register.FunctionRenderCollection.TryGetValue(item.GetType(), out var functionGroup))
+                {
+
+                    //var funClass = _serviceProvider.GetService(funtionBag.MethodInfo.DeclaringType) as IAppService;
+                    //var funMethod = funClass.GetType().GetMethod()
+
+                    //遍历某个 Register 下所有的方法      TODO：未来可添加分组
+                    foreach (var funtionBag in functionGroup.Values)
+                    {
+                        var result = await FunctionHelper.GetFunctionParameterInfoAsync(this._serviceProvider, funtionBag, true);
+
+                        registerItem.RegisterFunctionInfoList.Add(new()
+                        {
+                            Key = funtionBag.Key,
+                            Name = funtionBag.FunctionRenderAttribute.Name,
+                            Description = funtionBag.FunctionRenderAttribute.Description,
+                            FunctionRenderBag = funtionBag,
+                            FunctionParameterInfoList = result
+                        });
+                    }
+                }
+            }
         }
+
     }
 }
