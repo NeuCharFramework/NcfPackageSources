f8c203f13507b2d5d93713aee974dffe6b19d646
苏震巍 <zsu@senparc.com>
2024-05-14 17:40:34 +0800

    PromptRange v0.6.4.15 补充 Migration 数据库类型

---
+++ src/Extensions/Senparc.Xncf.PromptRange/Domain/Models/DatabaseModel/Mapping/PromptItemConfigurationMapping.cs
@@ -0,0 +1,13 @@
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Microsoft.EntityFrameworkCore;using System.Xml;using Microsoft.EntityFrameworkCore.Metadata.Builders;using Senparc.Ncf.Core.Models.DataBaseModel;namespace Senparc.Xncf.PromptRange.Domain.Models.DatabaseModel.Mapping{    internal class PromptPromptItemConfigurationMapping : ConfigurationMappingWithIdBase<PromptItem, int>    {        public override void Configure(EntityTypeBuilder<PromptItem> builder)
+        {
+            builder.Property(e => e.EvalAvgScore)
+                   .HasConversion<decimal>()
+                   .HasColumnType("NUMERIC");
+
+            builder.Property(e => e.EvalMaxScore)
+                   .HasConversion<decimal>()
+                   .HasColumnType("NUMERIC");
+
+
+            base.Configure(builder);
+        }    }}
