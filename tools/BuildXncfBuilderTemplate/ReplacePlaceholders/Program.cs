44314c993f463b59cd4431c1c807663baffd678f
苏震巍 <zsu@senparc.com>
2024-06-29 23:40:48 +0800

    BuildXncfBuilderTemplate 工具更新 string content = File.ReadAllText(filePath); 方法，避免无需修改的文件发生改写。

--- tools/BuildXncfBuilderTemplate/ReplacePlaceholders/Program.cs
+++ tools/BuildXncfBuilderTemplate/ReplacePlaceholders/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 string directoryPath;
+
 // 检查传入的参数  
 if (args.Length != 1)
 {
     Console.WriteLine("Usage: ReplacePlaceholders <directory>");
     directoryPath = "X:\\Senparc 项目\\NeuCharFramework\\NcfPackageSources\\src\\Extensions\\Senparc.Xncf.XncfBuilder\\Senparc.Xncf.XncfBuilder.Template";
 }
-
-directoryPath = args[0];
+else
+{
+    directoryPath = args[0];
+}
 
 if (!Directory.Exists(directoryPath))
 {
@@ -38,10 +42,30 @@ void ReplacePlaceholdersInDirectory(string directoryPath)
 
 void ReplacePlaceholdersInFile(string filePath)
 {
-    string content = File.ReadAllText(filePath);
-    content = content.Replace("ORGPLACEHOLDER", "Template_OrgName")
-                     .Replace("MODPLACEHOLDER", "Template_XncfName");
-    File.WriteAllText(filePath, content);
+    // 使用默认编码读取文件内容  
+    string content;
+    string newContent;
+    Encoding encoding;
+
+    // 读取文件内容和编码  
+    using (var reader = new StreamReader(filePath, true))
+    {
+        content = reader.ReadToEnd();
+        encoding = reader.CurrentEncoding;
+    }
+
+    newContent = content.Replace("ORGPLACEHOLDER", "Template_OrgName")
+                        .Replace("MODPLACEHOLDER", "Template_XncfName");
+
+    // 只有当内容发生变化时才写入文件  
+    if (!content.Equals(newContent))
+    {
+        // 使用读取时的编码写入文件内容  
+        using (var writer = new StreamWriter(filePath, false, encoding))
+        {
+            writer.Write(newContent);
+        }
+    }
 }
 
 void RenameFilesAndDirectories(string directoryPath)
@@ -54,6 +78,7 @@ void RenameFilesAndDirectories(string directoryPath)
         if (newFilePath != filePath)
         {
             File.Move(filePath, newFilePath);
+            Console.WriteLine($"文件已更新：{filePath} -> {newFilePath}");
         }
     }
 
@@ -65,6 +90,7 @@ void RenameFilesAndDirectories(string directoryPath)
         if (newSubDirectoryPath != subDirectoryPath)
         {
             Directory.Move(subDirectoryPath, newSubDirectoryPath);
+            Console.WriteLine($"目录已更新：{subDirectoryPath} -> {newSubDirectoryPath}");
         }
 
         RenameFilesAndDirectories(newSubDirectoryPath);
