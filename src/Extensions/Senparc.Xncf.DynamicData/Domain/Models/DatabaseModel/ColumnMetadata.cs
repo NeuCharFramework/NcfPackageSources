1de46c57b04466280aa6ad5e462b27c80f74bcea
苏震巍 <zsu@senparc.com>
2024-07-18 23:09:41 +0800

    添加 Senparc.Xncf.DynamicData 模块

---
+++ src/Extensions/Senparc.Xncf.DynamicData/Domain/Models/DatabaseModel/ColumnMetadata.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic;
+using Senparc.Ncf.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Senparc.Xncf.DynamicData
+{
+    /// <summary>  
+    /// ColumnMetadata 实体类，用于存储列的基本信息。  
+    /// </summary>  
+    [Table(Register.DATABASE_PREFIX + nameof(ColumnMetadata))]
+    [Serializable]
+    public class ColumnMetadata : EntityBase<int>
+    {
+        /// <summary>  
+        /// 关联的表格ID。  
+        /// </summary>  
+        //[ForeignKey(nameof(TableMetadata))]
+        public int TableMetadataId { get; private set; }
+
+        /// <summary>  
+        /// 列名称。  
+        /// </summary>  
+        [Required]
+        [MaxLength(255)]
+        public string ColumnName { get; private set; }
+
+        /// <summary>  
+        /// 列的数据类型。  
+        /// </summary>  
+        [Required]
+        [MaxLength(50)]
+        public string ColumnType { get; private set; }
+
+        /// <summary>  
+        /// 是否允许NULL值。  
+        /// </summary>  
+        [Required]
+        public bool IsNullable { get; private set; } = true;
+
+        /// <summary>  
+        /// 列的默认值。  
+        /// </summary>  
+        public string DefaultValue { get; private set; }
+
+        /// <summary>  
+        /// 关联的表格元数据。  
+        /// </summary>  
+        //[InverseProperty(nameof(TableMetadata.ColumnMetadatas))]
+        public TableMetadata TableMetadata { get;  set; }
+
+        /// <summary>  
+        /// 关联的表格元数据。  
+        /// </summary>  
+        //[InverseProperty(nameof(TableData.ColumnMetadata))]
+        public ICollection<TableData> TableDatas { get;  set; }
+
+        private ColumnMetadata() { }
+        public ColumnMetadata(string columnName, string columnType, bool isNullable, string defaultValue)
+        {
+            ColumnName = columnName;
+            ColumnType = columnType;
+            IsNullable = isNullable;
+            DefaultValue = defaultValue;
+        }
+    }
+}
---
+++ src/Extensions/Senparc.Xncf.DynamicData/Domain/Models/DatabaseModel/TableMetadata.cs
@@ -0,0 +1,49 @@
+﻿using Senparc.Ncf.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Senparc.Xncf.DynamicData
+{
+    /// <summary>  
+    /// TableMetadata 实体类，用于存储表格的基本信息。  
+    /// </summary>  
+    [Table(Register.DATABASE_PREFIX + nameof(TableMetadata))]
+    [Serializable]
+    public class TableMetadata : EntityBase<int>
+    {
+        /// <summary>  
+        /// 表格名称。  
+        /// </summary>  
+        [Required]
+        [MaxLength(255)]
+        public string TableName { get; private set; }
+
+        /// <summary>  
+        /// 表格描述。  
+        /// </summary>  
+        public string Description { get; private set; }
+
+        /// <summary>  
+        /// 关联的列元数据集合。  
+        /// </summary>  
+        //[InverseProperty(nameof(ColumnMetadata.TableMetadata))]
+        public ICollection<ColumnMetadata> ColumnMetadatas { get; set; }
+
+        /// <summary>  
+        /// 关联的数据集合。  
+        /// </summary>  
+        //[InverseProperty(nameof(TableData.TableMetadata))]
+        public ICollection<TableData> TableDatas { get; set; }
+
+        private TableMetadata() { }
+
+        public TableMetadata(string tableName, string description)
+        {
+            TableName = tableName;
+            Description = description;
+        }
+
+    }
+}
