e7a37eebac6429fd554b9bff3bb6976ca1fedc88
JeffreySu <zsu@senparc.com>
2024-10-17 12:55:17 +0800

    完成 ChatTask 接口

---
+++ src/Extensions/Senparc.Xncf.AgentsManager/Domain/Services/ChatGroupHistoryService.cs
@@ -0,0 +1,25 @@
+﻿using Senparc.Ncf.Repository;
+using Senparc.Ncf.Service;
+using Senparc.Xncf.AgentsManager.Models.DatabaseModel.Models;
+using Senparc.Xncf.AgentsManager.Models.DatabaseModel.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.AgentsManager.Domain.Services
+{
+    public class ChatGroupHistoryService : ServiceBase<ChatGroupHistory>
+    {
+        public ChatGroupHistoryService(IRepositoryBase<ChatGroupHistory> repo, IServiceProvider serviceProvider) : base(repo, serviceProvider)
+        {
+        }
+
+        public async Task CreateHistory(ChatGroupHistoryDto chatGroupHistoryDto)
+        {
+            var chatGroupHistory = new ChatGroupHistory(chatGroupHistoryDto);
+            await base.SaveObjectAsync(chatGroupHistory);
+        }
+    }
+}
