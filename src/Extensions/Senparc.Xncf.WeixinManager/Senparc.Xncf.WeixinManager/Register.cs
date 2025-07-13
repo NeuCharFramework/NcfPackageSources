8a1d7052a96fe307afa662b444b88c7eed60d5be
JeffreySu <zsu@senparc.com>
2025-07-13 19:44:20 +0800

    WeixinManager v0.20.0-preview.1 upgrade MpAccount loading progress in UseXncfModule

--- src/Extensions/Senparc.Xncf.WeixinManager/Senparc.Xncf.WeixinManager/Register.cs
+++ src/Extensions/Senparc.Xncf.WeixinManager/Senparc.Xncf.WeixinManager/Register.cs
@@ -33,7 +33,7 @@ using System.Threading.Tasks;
 namespace Senparc.Xncf.WeixinManager
 {
     [XncfRegister]
-    [XncfOrder(5880)]
+    [XncfOrder(0)]
     public partial class Register : XncfRegisterBase, IXncfRegister //注册 XNCF 基础模块接口（必须）
     {
         #region IXncfRegister 接口
@@ -43,7 +43,7 @@ namespace Senparc.Xncf.WeixinManager
         public override string Uid => "EB84CB21-AC22-406E-0001-000000000001";
 
 
-        public override string Version => "0.22.0";
+        public override string Version => "0.23.0";
 
 
         public override string MenuName => "微信管理";
@@ -106,34 +106,48 @@ namespace Senparc.Xncf.WeixinManager
         }
 
 
+        private async Task InitWeixinDatabase(IApplicationBuilder app, Func<Task> task)
+        {
+
+
+            await task();
+        }
+
         public override IApplicationBuilder UseXncfModule(IApplicationBuilder app, IRegisterService registerService)
         {
             //注册微信
             Senparc.Weixin.WeixinRegister.UseSenparcWeixin(null, null, senparcSetting: null);
 
-            try
+            //等待数据库注册完成后运行
+            _ = Task.Factory.StartNew(async () =>
             {
-                //未安装数据库表的情况下可能会出错，因此需要try
-                using (var scope = app.ApplicationServices.CreateScope())
+                while (Ncf.Database.Register.UseNcfDatabaseSetted is false)
                 {
-                    var mpAccountService = scope.ServiceProvider.GetRequiredService<MpAccountService>();
-                    var allMpAccount = mpAccountService.GetAllMpAccounts();
+                    await Task.Delay(1000);
+                }
 
-                    //批量自动注册公众号
-                    foreach (var mpAccount in allMpAccount)
+                try
+                {
+                    //未安装数据库表的情况下可能会出错，因此需要try
+                    using (var scope = app.ApplicationServices.CreateScope())
                     {
-                        Task.Factory.StartNew(async () =>
-                        {
-                            await AccessTokenContainer.RegisterAsync(mpAccount.AppId, mpAccount.AppSecret, $"{mpAccount.Name}-{mpAccount.Id}");
-                        });
+                        var mpAccountService = scope.ServiceProvider.GetRequiredService<MpAccountService>();
+                        var allMpAccount = mpAccountService.GetAllMpAccounts();
 
-                        //TODO：更多执行过程中的动态注册
+                        //批量自动注册公众号
+                        allMpAccount.AsParallel().ForAll(mpAccount =>
+                        {
+                            AccessTokenContainer.RegisterAsync(mpAccount.AppId, mpAccount.AppSecret, $"{mpAccount.Name}-{mpAccount.Id}");
+                            //TODO：更多执行过程中的动态注册
+                        });
                     }
                 }
-            }
-            catch
-            {
-            }
+                catch (Exception ex)
+                {
+                    SenparcTrace.BaseExceptionLog(ex);
+                }
+
+            });
 
             //app.UseSwagger();
             //app.UseSwaggerUI(c =>
@@ -163,109 +177,98 @@ namespace Senparc.Xncf.WeixinManager
         }
         #endregion
 
-    }
+        class RemoveVerbsFilter : IDocumentFilter
+        {
+            //public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+            //{
+            //    foreach (PathItem path in swaggerDoc.paths.Values)
+            //    {
+            //        path.delete = null;
+            //        //path.get = null; // leaving GET in
+            //        path.head = null;
+            //        path.options = null;
+            //        path.patch = null;
+            //        path.post = null;
+            //        path.put = null;
+            //    }
+            //}
 
-    class RemoveVerbsFilter : IDocumentFilter
-    {
-        //public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+            public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+            {
+                //每次切换定义，都需要经过比较长的时间才到达这里
+
+                return;
+                string platformType;
+                var title = swaggerDoc.Info.Title;
+
+                if (title.Contains(PlatformType.WeChat_OfficialAccount.ToString()))
+                {
+                    platformType = PlatformType.WeChat_OfficialAccount.ToString();
+                }
+                else if (title.Contains(PlatformType.WeChat_Work.ToString()))
+                {
+                    platformType = PlatformType.WeChat_Work.ToString();
+                }
+                else if (title.Contains(PlatformType.WeChat_Open.ToString()))
+                {
+                    platformType = PlatformType.WeChat_Open.ToString();
+                }
+                else if (title.Contains(PlatformType.WeChat_MiniProgram.ToString()))
+                {
+                    platformType = PlatformType.WeChat_MiniProgram.ToString();
+                }
+                //else if (title.Contains(PlatformType.General.ToString()))
+                //{
+                //    platformType = PlatformType.General.ToString();
+                //}
+                else
+                {
+                    throw new NotImplementedException($"未提供的 PlatformType 类型，Title：{title}");
+                }
+
+                var pathList = swaggerDoc.Paths.Keys.ToList();
+
+                foreach (var path in pathList)
+                {
+                    if (!path.Contains(platformType))
+                    {
+                        //移除非当前模块的API对象
+                        swaggerDoc.Paths.Remove(path);
+                    }
+                }
+
+                //SwaggerOperationAttribute
+                //移除Schema对象
+                //var toRemoveSchema = context.SchemaRepository.Schemas.Where(z => !z.Key.Contains(platformType)).ToList();//结果为全部删除，仅测试
+                //foreach (var schema in toRemoveSchema)
+                //{
+                //    context.SchemaRepository.Schemas.Remove(schema.Key);
+                //}
+            }
+        }
+
+        //public class AuthResponsesOperationFilter : IOperationFilter
         //{
-        //    foreach (PathItem path in swaggerDoc.paths.Values)
+        //    public void Apply(OpenApiOperation operation, OperationFilterContext context)
         //    {
-        //        path.delete = null;
-        //        //path.get = null; // leaving GET in
-        //        path.head = null;
-        //        path.options = null;
-        //        path.patch = null;
-        //        path.post = null;
-        //        path.put = null;
+        //        //获取是否添加登录特性
+        //        var authAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+        //         .Union(context.MethodInfo.GetCustomAttributes(true))
+        //         .OfType<AuthorizeAttribute>().Any();
+
+        //        if (authAttributes)
+        //        {
+        //            operation.Responses.Add("401", new OpenApiResponse { Description = "暂无访问权限" });
+        //            operation.Responses.Add("403", new OpenApiResponse { Description = "禁止访问" });
+        //            operation.Security = new List<OpenApiSecurityRequirement>
+        //            {
+        //                new OpenApiSecurityRequirement { { new OpenApiSecurityScheme() {  Name= "oauth2" }, new[] { "swagger_api" } }}
+        //            };
+        //        }
         //    }
         //}
 
-        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
-        {
-            //每次切换定义，都需要经过比较长的时间才到达这里
-
-            return;
-            string platformType;
-            var title = swaggerDoc.Info.Title;
-
-            if (title.Contains(PlatformType.WeChat_OfficialAccount.ToString()))
-            {
-                platformType = PlatformType.WeChat_OfficialAccount.ToString();
-            }
-            else if (title.Contains(PlatformType.WeChat_Work.ToString()))
-            {
-                platformType = PlatformType.WeChat_Work.ToString();
-            }
-            else if (title.Contains(PlatformType.WeChat_Open.ToString()))
-            {
-                platformType = PlatformType.WeChat_Open.ToString();
-            }
-            else if (title.Contains(PlatformType.WeChat_MiniProgram.ToString()))
-            {
-                platformType = PlatformType.WeChat_MiniProgram.ToString();
-            }
-            //else if (title.Contains(PlatformType.General.ToString()))
-            //{
-            //    platformType = PlatformType.General.ToString();
-            //}
-            else
-            {
-                throw new NotImplementedException($"未提供的 PlatformType 类型，Title：{title}");
-            }
-
-            var pathList = swaggerDoc.Paths.Keys.ToList();
-
-            foreach (var path in pathList)
-            {
-                if (!path.Contains(platformType))
-                {
-                    //移除非当前模块的API对象
-                    swaggerDoc.Paths.Remove(path);
-                }
-            }
-
-            //SwaggerOperationAttribute
-            //移除Schema对象
-            //var toRemoveSchema = context.SchemaRepository.Schemas.Where(z => !z.Key.Contains(platformType)).ToList();//结果为全部删除，仅测试
-            //foreach (var schema in toRemoveSchema)
-            //{
-            //    context.SchemaRepository.Schemas.Remove(schema.Key);
-            //}
-        }
     }
-
-    //public class AuthResponsesOperationFilter : IOperationFilter
-    //{
-    //    public void Apply(OpenApiOperation operation, OperationFilterContext context)
-    //    {
-    //        //获取是否添加登录特性
-    //        var authAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
-    //         .Union(context.MethodInfo.GetCustomAttributes(true))
-    //         .OfType<AuthorizeAttribute>().Any();
-
-    //        if (authAttributes)
-    //        {
-    //            operation.Responses.Add("401", new OpenApiResponse { Description = "暂无访问权限" });
-    //            operation.Responses.Add("403", new OpenApiResponse { Description = "禁止访问" });
-    //            operation.Security = new List<OpenApiSecurityRequirement>
-    //            {
-    //                new OpenApiSecurityRequirement { { new OpenApiSecurityScheme() {  Name= "oauth2" }, new[] { "swagger_api" } }}
-    //            };
-    //        }
-    //    }
-    //}
-
 }
-
-
-
-
-
-
-
-
-
-
 
 
