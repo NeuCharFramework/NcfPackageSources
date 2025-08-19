f00d33da49a2b4557ea8719bd9d735b9e0a2e216
JeffreySu <zsu@senparc.com>
2025-08-20 00:54:49 +0800

    PromptRange v0.13.17-preview.1 remove debug code in prompt.js

--- src/Extensions/Senparc.Xncf.WeixinManager/Senparc.Xncf.WeixinManager/Register.cs
+++ src/Extensions/Senparc.Xncf.WeixinManager/Senparc.Xncf.WeixinManager/Register.cs
@@ -51,7 +51,7 @@ namespace Senparc.Xncf.WeixinManager
         public override string Uid => "EB84CB21-AC22-406E-0001-000000000001";
 
 
-        public override string Version => "0.23.0";
+        public override string Version => "0.21.1";
 
 
         public override string MenuName => "微信管理";
