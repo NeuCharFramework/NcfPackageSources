7e590fcd728aa4386fcab35a5c881ef2c3d58819
MLTechnology <zhao365845726@163.com>
2026-02-25 00:46:36 +0800

    Optimize recallTest function

---
+++ src/Extensions/Senparc.Xncf.KnowledgeBase/OHS/Local/PL/Request/RecallTestRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.KnowledgeBase.OHS.Local.PL.Request
+{
+    public class RecallTestRequest
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
---
+++ src/Extensions/Senparc.Xncf.KnowledgeBase/OHS/Local/PL/Response/RecallTestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.Xncf.KnowledgeBase.OHS.Local.PL.Response
+{
+    public class RecallTestResponse
+    {
+        public string Score { get; set; }
+        public string Content { get; set; }
+        public string RecallTime { get; set; }
+    }
+}
