55e5f1dea14acee4457b6ea07faafae1b6f027e9
MLTechnology <zhao365845726@163.com>
2026-03-01 22:08:52 +0800

    Optimize knowledgebase index page style and search function

--- src/Extensions/Senparc.Xncf.KnowledgeBase/Areas/Admin/Pages/KnowledgeBase/Index.cshtml.cs
+++ src/Extensions/Senparc.Xncf.KnowledgeBase/Areas/Admin/Pages/KnowledgeBase/Index.cshtml.cs
@@ -43,25 +43,26 @@ namespace Senparc.Xncf.KnowledgeBase.Areas.Admin.Pages.KnowledgeBase
         public async Task<IActionResult> OnGetKnowledgeBasesAsync(string keyword, string orderField, int pageIndex, int pageSize)
         {
             var seh = new SenparcExpressionHelper<Models.DatabaseModel.KnowledgeBase>();
-            //seh.ValueCompare.AndAlso(!string.IsNullOrEmpty(keyword), _ => _.Name.Contains(keyword));
+            seh.ValueCompare.AndAlso(!string.IsNullOrEmpty(keyword), _ => _.Name.Contains(keyword));
             var where = seh.BuildWhereExpression();
             var response = await _knowledgeBaseService.GetObjectListAsync(pageIndex, pageSize, where, orderField);
             return Ok(new
-                    {
-                        response.TotalCount,
-                        response.PageIndex,
-                        List = response.Select(_ => new {
-                            _.Id,
-                            _.LastUpdateTime,
-                            _.Remark,
-                            _.EmbeddingModelId,
-                            _.VectorDBId,
-                            _.ChatModelId,
-                            _.Name,
-                            _.Content,
-                            _.AddTime
-                        })
-                    });
+            {
+                response.TotalCount,
+                response.PageIndex,
+                List = response.Select(_ => new
+                {
+                    _.Id,
+                    _.LastUpdateTime,
+                    _.Remark,
+                    _.EmbeddingModelId,
+                    _.VectorDBId,
+                    _.ChatModelId,
+                    _.Name,
+                    _.Content,
+                    _.AddTime
+                })
+            });
         }
     }
 }
