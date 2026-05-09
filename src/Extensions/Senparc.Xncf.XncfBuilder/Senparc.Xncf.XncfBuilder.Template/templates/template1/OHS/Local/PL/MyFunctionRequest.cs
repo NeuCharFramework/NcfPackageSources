f3337eb99d252ab176b79acc70c6339f8cb15145
JeffreySu <zsu@senparc.com>
2026-05-10 00:53:02 +0800

    fix: Correct TargetFramework property in project files for consistency
    - Changed TargetFrameworks to TargetFramework in Senparc.Xncf.AgentsManager.Abstractions and Senparc.Xncf.PromptRange.Abstractions project files for standardization.
    - Adjusted formatting in MyFunctionRequest.cs to improve readability and maintain consistency in property definitions.

--- src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder.Template/templates/template1/OHS/Local/PL/MyFunctionRequest.cs
+++ src/Extensions/Senparc.Xncf.XncfBuilder/Senparc.Xncf.XncfBuilder.Template/templates/template1/OHS/Local/PL/MyFunctionRequest.cs
@@ -23,26 +23,26 @@ namespace Template_OrgName.Xncf.Template_XncfName.OHS.Local.PL
         [Description("数字||数字2")]
         public int Number2 { get; set; }
 
-        [Description("运算符||")]//下拉列表
-           [FunctionParameterUi(ParameterType.DropDownList, nameof(OperatorOptions))]
-           public string Operator { get; set; }
+        [Description("运算符||")]
+        [FunctionParameterUi(ParameterType.DropDownList, nameof(OperatorOptions))]
+        public string Operator { get; set; }
 
-           [JsonIgnore]
-           public SelectionList OperatorOptions { get; set; } = new SelectionList(SelectionType.DropDownList, new[] {
-                 new SelectionItem("+","加法","数字1 + 数字2",false),
-                 new SelectionItem("-","减法","数字1 - 数字2",true),
-                 new SelectionItem("×","乘法","数字1 × 数字2",false),
-                 new SelectionItem("÷","除法","数字1 ÷ 数字2",false)
-            });
+        [JsonIgnore]//下拉列表
+        public SelectionList OperatorOptions { get; set; } = new SelectionList(SelectionType.DropDownList, new[] {
+                new SelectionItem("+","加法","数字1 + 数字2",false),
+                new SelectionItem("-","减法","数字1 - 数字2",true),
+                new SelectionItem("×","乘法","数字1 × 数字2",false),
+                new SelectionItem("÷","除法","数字1 ÷ 数字2",false)
+        });
 
         [Description("计算平方||")]//多选框
-           [FunctionParameterUi(ParameterType.CheckBoxList, nameof(PowerOptions))]
-           public string[] Power { get; set; }
+        [FunctionParameterUi(ParameterType.CheckBoxList, nameof(PowerOptions))]
+        public string[] Power { get; set; }
 
-           [JsonIgnore]
-           public SelectionList PowerOptions { get; set; } = new SelectionList(SelectionType.CheckBoxList, new[] {
-                 new SelectionItem("2","平方","计算上述结果之后再计算平方",false),
-                 new SelectionItem("3","三次方","计算上述结果之后再计算三次方",false)
-            });
+        [JsonIgnore]
+        public SelectionList PowerOptions { get; set; } = new SelectionList(SelectionType.CheckBoxList, new[] {
+                new SelectionItem("2","平方","计算上述结果之后再计算平方",false),
+                new SelectionItem("3","三次方","计算上述结果之后再计算三次方",false)
+        });
     }
 }
